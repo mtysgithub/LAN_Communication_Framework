48d607b9c3850a21ac7284cd910b6ff527550185
yanci <0xmty.007@gmail.com>
2014-05-21 01:26:23 +0800

    0#一个重命名;消息模块增加点对点验证信息类

---
+++ Middleware/Communication/Package/Internal/C2CMessageVerificationReplyPackage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Middleware.Communication.Package.Internal
+{
+    class C2CMessageVerificationReplyPackage
+    {
+    }
+}
