1da2eb93956a17156fd8299d1315eb6c542ebbab
yanci <0xmty.007@gmail.com>
2014-05-21 02:49:30 +0800

    0#C2CMessageRadioPackage加入ICCSerializeOperat<>接口

--- Middleware/Communication/Package/Internal/C2CMessageRadioPackage.cs
+++ Middleware/Communication/Package/Internal/C2CMessageRadioPackage.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+
+using ProtocolLibrary.CCProtocol;
+
 using Middleware.Device;
 using Middleware.Communication.Message;
+using Middleware.Interface;
+
 namespace Middleware.Communication.Package.Internal
 {
-    public class C2CMessageRadioPackage : C2CRadioPackage
+    public class C2CMessageRadioPackage : C2CRadioPackage, ICCSerializeOperat<CCCommunicateClass.Seria_C2CMessageRadioPackage>
     {
         protected BaseMessage mMessage = null;
         public BaseMessage Message
@@ -18,6 +23,21 @@ namespace Middleware.Communication.Package.Internal
             : base(targetDevice, "C2CMessageRadioPackage_" + msg.Type.Name, null)
         {
             mMessage = msg;
+        }
+
+        public override byte[] SerializeMiddlewareMessage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public new CCCommunicateClass.Seria_C2CMessageRadioPackage ExportSerializeData()
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
+
+        public void ParseSerializeData(CCCommunicateClass.Seria_C2CMessageRadioPackage obj)
+        {
+            throw new Exception("The method or operation is not implemented.");
         }
     }
 }
